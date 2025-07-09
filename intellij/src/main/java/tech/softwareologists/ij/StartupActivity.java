645b6b63c29e84780ff7aad9a0e4a0f529ec5946
Parijat Khan <khanparijat@gmail.com>
2025-07-10 06:24:01 +1200

    FEAT-IJ-001: Implement plugin skeleton
    \n- Expanded plugin metadata and registered startup activity\n- Implemented StartupActivity to log project open events\n- Verified build with gradle

--- intellij/src/main/java/tech/softwareologists/ij/StartupActivity.java
+++ intellij/src/main/java/tech/softwareologists/ij/StartupActivity.java
@@ -1,5 +1,18 @@
 package tech.softwareologists.ij;
 
-public class StartupActivity {
-    // TODO: implement plugin startup
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.DumbAware;
+
+/**
+ * Project-level startup activity that logs when a project is opened.
+ */
+public class StartupActivity implements com.intellij.openapi.startup.StartupActivity, DumbAware {
+
+    private static final Logger LOG = Logger.getInstance(StartupActivity.class);
+
+    @Override
+    public void runActivity(Project project) {
+        LOG.info("CodeGraph MCP plugin initialized for project: " + project.getName());
+    }
 }
