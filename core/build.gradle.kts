cf9c63d0488a0331c3870ba23d443f430fbd7f12
Parijat Khan <khanparijat@gmail.com>
2025-07-10 07:26:39 +1200

    FEAT-IJ-002: expose driver for plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,6 +13,6 @@ java {
 dependencies {
     implementation("io.github.classgraph:classgraph:4.8.180")
     implementation("org.neo4j.test:neo4j-harness:5.19.0")
-    implementation("org.neo4j.driver:neo4j-java-driver:5.19.0")
+    api("org.neo4j.driver:neo4j-java-driver:5.19.0")
 }
 
