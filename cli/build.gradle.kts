cd95de02d78a1fb5f80d096bc2c7997820072436
Parijat Khan <khanparijat@gmail.com>
2025-07-11 16:15:55 +1200

    FEAT-CLI-001: support additional queries

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -18,5 +18,6 @@ dependencies {
     implementation("org.neo4j.test:neo4j-harness:5.19.0")
     implementation("org.neo4j.driver:neo4j-java-driver:5.19.0")
     implementation("io.github.classgraph:classgraph:4.8.180")
+    implementation("org.json:json:20240303")
 }
 
