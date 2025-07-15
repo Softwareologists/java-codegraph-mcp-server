b2d628fd31785d406b1fda24c49cb460e4ad5df7
Parijat Khan <khanparijat@gmail.com>
2025-07-16 06:44:13 +1200

    chore(ci): update release workflow to bump versions

---
+++ scripts/update-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+NEW_VERSION="$1"
+
+# Update ManifestGenerator.java
+sed -i "s/1\.0\.1/${NEW_VERSION}/" core/src/main/java/tech/softwareologists/core/ManifestGenerator.java
+
+# Update manifest.json.tpl
+sed -i -E "s/(\"version\": \")[0-9]+\.[0-9]+\.[0-9]+(\")/\1${NEW_VERSION}\2/" core/manifest.json.tpl
+
+# Update IntelliJ plugin.xml
+sed -i -E "s/(<version>)[0-9]+\.[0-9]+\.[0-9]+(<\/version>)/\1${NEW_VERSION}\2/" intellij/resources/META-INF/plugin.xml
+
+# Update distribution file reference in docs
+sed -i -E "s/(CodeGraphMcp-)[0-9]+\.[0-9]+\.[0-9]+(\.zip)/\1${NEW_VERSION}\2/" examples/idea-launch.md
+
+echo "Updated version to ${NEW_VERSION}"
